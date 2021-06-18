5cf0237edacfb709766154f43463de729220656a
YashSaysHi <84657510+YashSaysHi@users.noreply.github.com>
2021-06-18 23:03:05 +0530

    Update test_average.c

--- test_average.c
+++ test_average.c
@@ -12,12 +12,3 @@ int main(void) {
   assert (average(y, 1) == 5.0);
   printf("Average: passed\n");
   }
-float average(int a[],int n)
-{
-float sum=0;
-for(int i=0;i<n;i++)
- {
- sum=sum+a[i];
- }
-return sum/n;
-}
